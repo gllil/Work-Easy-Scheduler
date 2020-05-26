074095d493e22a1b330eeedf37a822399e6616ef
Gary Llil <garyjllil@gmail.com>
2020-05-26 14:48:25 -0400

    Added folders for components and pages. Added router paths.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import AdminHome from "./pages/admin/AdminHome";
+import Employees from "./pages/admin/Employees";
+import Hiring from "./pages/admin/Hiring";
+import AdminLogin from "./pages/admin/AdminLogin";
+import AdminRegister from "./pages/admin/AdminRegister";
+import Schedules from "./pages/admin/Schedules";
+import EmployeeHome from "./pages/employee/EmployeeHome";
+import EmployeeLogin from "./pages/employee/EmployeeLogin";
+import EmployeeRegister from "./pages/employee/EmployeeRegister";
+import TimeOff from "./pages/employee/TimeOff";
+import Trade from "./pages/employee/Trade";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/adminhome" component={AdminHome} />
+        <Route path="/employee" component={Employees} />
+        <Route path="/hiring" component={Hiring} />
+        <Route path="/adminlogin" component={AdminLogin} />
+        <Route path="/adminregister" component={AdminRegister} />
+        <Route path="/schedules" component={Schedules} />
+        <Route path="/employeehome" component={EmployeeHome} />
+        <Route path="/employeelogin" component={EmployeeLogin} />
+        <Route path="/employeeregister" component={EmployeeRegister} />
+        <Route path="/timeoff" component={TimeOff} />
+        <Route path="/trade" component={Trade} />
+      </Switch>
+    </Router>
   );
 }
 
